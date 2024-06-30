34eb0c076a2b8e8fcfc14b169958ec6f985f7166
Daniel Marlow <marlow@princeton.edu>
2024-06-30 09:50:14 -0400

    New DAQ.grc for N310

--- DAQ.py
+++ DAQ.py
@@ -29,7 +29,7 @@ import math
 
 class DAQ(gr.top_block):
 
-    def __init__(self, base_name='Doppler', decimation_factor=10000, f1=1.418e9, f2=1418e6, f3=1418e6, f4=1418e6, fft_size=2048, g1=75, g2=75, g3=75, g4=75, samp_rate=1.0e7, seconds=3600):
+    def __init__(self, base_name='Doppler', decimation_factor=10000, device="type=n3xx,product=n310,addr=192.168.20.2", fft_size=2048, frequency=1420.4058e6, mclock=125e6, pps='gpsdo', refclock='gpsdo', rfgain=65, samp_rate=1.25e7, seconds=3600, subdev="A:0 A:1"):
         gr.top_block.__init__(self, "DAQ", catch_exceptions=True)
 
         ##################################################
@@ -37,17 +37,16 @@ class DAQ(gr.top_block):
         ##################################################
         self.base_name = base_name
         self.decimation_factor = decimation_factor
-        self.f1 = f1
-        self.f2 = f2
-        self.f3 = f3
-        self.f4 = f4
+        self.device = device
         self.fft_size = fft_size
-        self.g1 = g1
-        self.g2 = g2
-        self.g3 = g3
-        self.g4 = g4
+        self.frequency = frequency
+        self.mclock = mclock
+        self.pps = pps
+        self.refclock = refclock
+        self.rfgain = rfgain
         self.samp_rate = samp_rate
         self.seconds = seconds
+        self.subdev = subdev
 
         ##################################################
         # Variables
@@ -59,37 +58,31 @@ class DAQ(gr.top_block):
         ##################################################
 
         self.uhd_usrp_source_0 = uhd.usrp_source(
-            ",".join(('addr=192.168.40.2,type=x300', 'master_clock_rate=200e6', "master_clock_rate=200e6")),
+            ",".join((device, "master_clock_rate=%f" % mclock)),
             uhd.stream_args(
                 cpu_format="fc32",
                 args='',
                 channels=list(range(0,2)),
             ),
         )
-        self.uhd_usrp_source_0.set_clock_source('external', 0)
-        self.uhd_usrp_source_0.set_subdev_spec("A:0 A:1", 0)
+        self.uhd_usrp_source_0.set_clock_source(refclock, 0)
+        self.uhd_usrp_source_0.set_time_source(pps, 0)
+        self.uhd_usrp_source_0.set_subdev_spec(subdev, 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        _last_pps_time = self.uhd_usrp_source_0.get_time_last_pps().get_real_secs()
-        # Poll get_time_last_pps() every 50 ms until a change is seen
-        while(self.uhd_usrp_source_0.get_time_last_pps().get_real_secs() == _last_pps_time):
-            time.sleep(0.05)
-        # Set the time to PC time on next PPS
-        self.uhd_usrp_source_0.set_time_next_pps(uhd.time_spec(int(time.time()) + 1.0))
+        # Set the time to GPS time on next PPS
+        # get_mboard_sensor("gps_time") returns just after the PPS edge,
+        # thus add one second and set the time on the next PPS
+        self.uhd_usrp_source_0.set_time_next_pps(uhd.time_spec(self.uhd_usrp_source_0.get_mboard_sensor("gps_time").to_int() + 1.0))
         # Sleep 1 second to ensure next PPS has come
         time.sleep(1)
 
-        self.uhd_usrp_source_0.set_center_freq(f1, 0)
-        self.uhd_usrp_source_0.set_antenna('RX1', 0)
-        self.uhd_usrp_source_0.set_gain(g1, 0)
+        self.uhd_usrp_source_0.set_center_freq(frequency, 0)
+        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self.uhd_usrp_source_0.set_gain(rfgain, 0)
 
-        self.uhd_usrp_source_0.set_lo_source('internal', uhd.ALL_LOS, 0)
-        self.uhd_usrp_source_0.set_lo_export_enabled(True, uhd.ALL_LOS, 0)
-        self.uhd_usrp_source_0.set_center_freq(f2, 1)
-        self.uhd_usrp_source_0.set_antenna('RX2', 1)
-        self.uhd_usrp_source_0.set_gain(g2, 1)
-
-        self.uhd_usrp_source_0.set_lo_source('companion', uhd.ALL_LOS, 1)
-        self.uhd_usrp_source_0.set_lo_export_enabled(False, uhd.ALL_LOS, 1)
+        self.uhd_usrp_source_0.set_center_freq(frequency, 1)
+        self.uhd_usrp_source_0.set_antenna("RX2", 1)
+        self.uhd_usrp_source_0.set_gain(rfgain, 1)
         self.single_pole_iir_filter_xx_0_0 = filter.single_pole_iir_filter_ff(alpha_IIR, fft_size)
         self.single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff(alpha_IIR, fft_size)
         self.rx_time_catcher = rx_time_catcher.blk(rxfile=base_name + "_ts.txt", fftsize=32, crate=samp_rate/fft_size/decimation_factor, srate=samp_rate)
@@ -147,33 +140,11 @@ class DAQ(gr.top_block):
         self.blocks_keep_one_in_n_0.set_n(self.decimation_factor)
         self.blocks_keep_one_in_n_0_0.set_n(self.decimation_factor)
 
-    def get_f1(self):
-        return self.f1
+    def get_device(self):
+        return self.device
 
-    def set_f1(self, f1):
-        self.f1 = f1
-        self.uhd_usrp_source_0.set_center_freq(self.f1, 0)
-
-    def get_f2(self):
-        return self.f2
-
-    def set_f2(self, f2):
-        self.f2 = f2
-        self.uhd_usrp_source_0.set_center_freq(self.f2, 1)
-
-    def get_f3(self):
-        return self.f3
-
-    def set_f3(self, f3):
-        self.f3 = f3
-        self.uhd_usrp_source_0.set_center_freq(self.f3, 2)
-
-    def get_f4(self):
-        return self.f4
-
-    def set_f4(self, f4):
-        self.f4 = f4
-        self.uhd_usrp_source_0.set_center_freq(self.f4, 3)
+    def set_device(self, device):
+        self.device = device
 
     def get_fft_size(self):
         return self.fft_size
@@ -181,33 +152,39 @@ class DAQ(gr.top_block):
     def set_fft_size(self, fft_size):
         self.fft_size = fft_size
 
-    def get_g1(self):
-        return self.g1
+    def get_frequency(self):
+        return self.frequency
 
-    def set_g1(self, g1):
-        self.g1 = g1
-        self.uhd_usrp_source_0.set_gain(self.g1, 0)
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+        self.uhd_usrp_source_0.set_center_freq(self.frequency, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.frequency, 1)
 
-    def get_g2(self):
-        return self.g2
+    def get_mclock(self):
+        return self.mclock
 
-    def set_g2(self, g2):
-        self.g2 = g2
-        self.uhd_usrp_source_0.set_gain(self.g2, 1)
+    def set_mclock(self, mclock):
+        self.mclock = mclock
 
-    def get_g3(self):
-        return self.g3
+    def get_pps(self):
+        return self.pps
 
-    def set_g3(self, g3):
-        self.g3 = g3
-        self.uhd_usrp_source_0.set_gain(self.g3, 2)
+    def set_pps(self, pps):
+        self.pps = pps
 
-    def get_g4(self):
-        return self.g4
+    def get_refclock(self):
+        return self.refclock
 
-    def set_g4(self, g4):
-        self.g4 = g4
-        self.uhd_usrp_source_0.set_gain(self.g4, 3)
+    def set_refclock(self, refclock):
+        self.refclock = refclock
+
+    def get_rfgain(self):
+        return self.rfgain
+
+    def set_rfgain(self, rfgain):
+        self.rfgain = rfgain
+        self.uhd_usrp_source_0.set_gain(self.rfgain, 0)
+        self.uhd_usrp_source_0.set_gain(self.rfgain, 1)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -223,6 +200,12 @@ class DAQ(gr.top_block):
     def set_seconds(self, seconds):
         self.seconds = seconds
         self.blocks_head_0.set_length((int(self.seconds*self.samp_rate)))
+
+    def get_subdev(self):
+        return self.subdev
+
+    def set_subdev(self, subdev):
+        self.subdev = subdev
 
     def get_alpha_IIR(self):
         return self.alpha_IIR
@@ -243,45 +226,42 @@ def argument_parser():
         "--decimation-factor", dest="decimation_factor", type=intx, default=10000,
         help="Set Decimation Factor [default=%(default)r]")
     parser.add_argument(
-        "--f1", dest="f1", type=eng_float, default=eng_notation.num_to_str(float(1.418e9)),
-        help="Set Chan 1 freq [default=%(default)r]")
-    parser.add_argument(
-        "--f2", dest="f2", type=eng_float, default=eng_notation.num_to_str(float(1418e6)),
-        help="Set Chan 2 freq [default=%(default)r]")
-    parser.add_argument(
-        "--f3", dest="f3", type=eng_float, default=eng_notation.num_to_str(float(1418e6)),
-        help="Set Chan 3 freq [default=%(default)r]")
-    parser.add_argument(
-        "--f4", dest="f4", type=eng_float, default=eng_notation.num_to_str(float(1418e6)),
-        help="Set Chan 4 freq [default=%(default)r]")
+        "--device", dest="device", type=str, default="type=n3xx,product=n310,addr=192.168.20.2",
+        help="Set Device address string [default=%(default)r]")
     parser.add_argument(
         "--fft-size", dest="fft_size", type=intx, default=2048,
         help="Set fft_size [default=%(default)r]")
     parser.add_argument(
-        "--g1", dest="g1", type=eng_float, default=eng_notation.num_to_str(float(75)),
-        help="Set Chan 1 gain [default=%(default)r]")
+        "--frequency", dest="frequency", type=eng_float, default=eng_notation.num_to_str(float(1420.4058e6)),
+        help="Set Center Frequency (Hz) [default=%(default)r]")
     parser.add_argument(
-        "--g2", dest="g2", type=eng_float, default=eng_notation.num_to_str(float(75)),
-        help="Set Chan 2 gain [default=%(default)r]")
+        "--mclock", dest="mclock", type=eng_float, default=eng_notation.num_to_str(float(125e6)),
+        help="Set Master Clock Rate [default=%(default)r]")
     parser.add_argument(
-        "--g3", dest="g3", type=eng_float, default=eng_notation.num_to_str(float(75)),
-        help="Set Chan 3 gain [default=%(default)r]")
+        "--pps", dest="pps", type=str, default='gpsdo',
+        help="Set PPS Clock SOurce [default=%(default)r]")
     parser.add_argument(
-        "--g4", dest="g4", type=eng_float, default=eng_notation.num_to_str(float(75)),
-        help="Set Chan 4 gain [default=%(default)r]")
+        "--refclock", dest="refclock", type=str, default='gpsdo',
+        help="Set Reference clock type [default=%(default)r]")
     parser.add_argument(
-        "--samp-rate", dest="samp_rate", type=eng_float, default=eng_notation.num_to_str(float(1.0e7)),
+        "--rfgain", dest="rfgain", type=eng_float, default=eng_notation.num_to_str(float(65)),
+        help="Set RF Gain [default=%(default)r]")
+    parser.add_argument(
+        "--samp-rate", dest="samp_rate", type=eng_float, default=eng_notation.num_to_str(float(1.25e7)),
         help="Set Sample Rate [default=%(default)r]")
     parser.add_argument(
         "--seconds", dest="seconds", type=intx, default=3600,
         help="Set Seconds [default=%(default)r]")
+    parser.add_argument(
+        "--subdev", dest="subdev", type=str, default="A:0 A:1",
+        help="Set Sub-device spec [default=%(default)r]")
     return parser
 
 
 def main(top_block_cls=DAQ, options=None):
     if options is None:
         options = argument_parser().parse_args()
-    tb = top_block_cls(base_name=options.base_name, decimation_factor=options.decimation_factor, f1=options.f1, f2=options.f2, f3=options.f3, f4=options.f4, fft_size=options.fft_size, g1=options.g1, g2=options.g2, g3=options.g3, g4=options.g4, samp_rate=options.samp_rate, seconds=options.seconds)
+    tb = top_block_cls(base_name=options.base_name, decimation_factor=options.decimation_factor, device=options.device, fft_size=options.fft_size, frequency=options.frequency, mclock=options.mclock, pps=options.pps, refclock=options.refclock, rfgain=options.rfgain, samp_rate=options.samp_rate, seconds=options.seconds, subdev=options.subdev)
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
