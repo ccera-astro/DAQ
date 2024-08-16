307be8f65d4fb8528e7512bb3f367b24bd73228a
Daniel Marlow <marlow@princeton.edu>
2024-08-16 15:15:57 -0400

    Update README.  Tweak runDAQ.py

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # DAQ
-Data acquisition scripts
+
+## Data acquisition scripts.    
+The principal modules are DAQ.py,
+which is the Python representation of the DAQ.grc flowgraph;
+and runDAQ.py, which is used to launch DAQ.py.   Although DAQ.py
+can be run directly, it is generally more convenient to use
+runDAQ.py.
+
+### Run modes 
+`runDAQ.py` runs in four basic "modes": pulsar, doppler, scan, or generic.
+The mode is specified with the `run_mode` parameter.
+The choice of mode determines the default values of other run time parameters,
+such as `f_center, fft_size, samp_rate, decimation_factor`, etc.    These 
+defaults, can be overridden by specifying them explicitly at run time.
+
+### Other runDAQ.py parameters 
+
+- `run_type`: which should be either "track" or "transit"
+- `run_time`: the total run time in integer seconds
+- `target`: the astrophysical target, e.g., M31  
+  Note that setting this parameter to "test" or "junk" will lead to eventual deletion.
+- `lmst`: wait until this time in LMST (specified in hours) before starting the DAQ
+- `XMLRPC`: enable control over file name changing via XMLRPC
+- `no_avg`: don't compute the average PSD (see below)
+- `no_sum`: don't compute the sum time series (see below)
+
+### Data formats
+
+The base file name of a given dataset is a time stamp of the form YYYY-MM-DD-mmmm 
+followed by either "_1" or "_2" corresponding to the receiver channel.
+
+The following file extensions are used:
+
+- `.json`  This is JSON file containing the run metadata
+- `.raw`   This is a time series of vectors (FFT PSDs)
+- `.avg`   This is a PSD averaged over the full .raw file
+- `.sum`   This is a time series corresponding to the sum 
+         of the PSDs in the .raw file.
+
+During data taking, the output of the DAQ flowgraph is written to the disk 
+in the form of series of vectors (FFT PSDs) (the `.raw` file).  By default, to save 
+disk space, at the end of each run, either a `.avg` file (for Doppler mode) or
+a `.sum` file (for Pulsar mode) is computed and <span style="color:red"> the .raw file is deleted. </span>
+To avoid this, the --no_avg or --no_sum parameters can be used. 
+
+
+
 
--- runDAQ.py
+++ runDAQ.py
@@ -6,9 +6,14 @@ import sys
 import signal
 from math import degrees
 import ephem 
+import numpy as np 
+import os 
+ 
 
 def getArgs() :
     import argparse
+    from pathlib import Path
+    home = str(Path.home())
     parser = argparse.ArgumentParser()
     parser.add_argument("-r","--run_mode",default="none",help="Run mode: pulsar, doppler, scan, or generic")
     parser.add_argument("--f_center",type=float,default=-1.,help="Center frequency")
@@ -17,10 +22,14 @@ def getArgs() :
     parser.add_argument("--decimation_factor",type=int,default=-1,help="Decimation factor")
     parser.add_argument("--run_type",default='Transit',help="Track or Transit")
     parser.add_argument("--target",default='none',help="Target, e.g. J0332+5434")
-    parser.add_argument("--dir",default="/home/dmarlow/data/",help="Data directory")
+    parser.add_argument("--dir",default=home + "/data/",help="Data directory")
     parser.add_argument("--run_time",type=int,default=600,help="Run time in seconds.")
     parser.add_argument("--printOn",action="store_true",help="Turn on print statements")
     parser.add_argument("--lmst",type=float,default=None,help="Wait for this lmst before starting.")
+    parser.add_argument("--n_jobs",type=int,default=1,help="Number of jobs to run.")
+    parser.add_argument("--no_avg",action="store_true",help="Don't make an .avg file")
+    parser.add_argument("--no_sum",action="store_true",help="Don't make a .sum file")
+    parser.add_argument("--XMLRPC",action="store_true",help="Enable XMLRPC control")
     return parser.parse_args()
 
 def getAzAlt() :
@@ -64,7 +73,7 @@ def E2G( RA, dec) :
     return degrees(gCoords.lon), degrees(gCoords.lat)
 
 #   build meta data dictionary 
-def buildMetadata(args,tb) :
+def buildMetadata(run_mode,target,tb) :
     dict = {}
     dict['freq'] = tb.get_frequency() 
     dict['srate'] = srate = tb.get_samp_rate() 
@@ -73,8 +82,8 @@ def buildMetadata(args,tb) :
     dict['decimation_factor'] = N 
     dict['t_sample'] = 1./(srate/fft_size/N)
     dict['n_chans'] = 2
-    dict['run_mode'] = args.run_mode  
-    dict['target'] = args.target 
+    dict['run_mode'] = run_mode  
+    dict['target'] = target 
     az, alt, az_rate, alt_rate = getAzAlt()
     dict['az'] = az
     dict['alt'] = alt
@@ -105,6 +114,35 @@ def signal_handler(sig, frame):
     time.sleep(1)
     endRun(metadata,file_base_name)
     sys.exit(0)
+
+def set_filename(file_name) :
+    print("Enter set_filename() file_name={0:s}".format(file_name))
+    file_base_name = tb.get_base_name() 
+    print("   Present file_base_name={0:s}".format(file_base_name))
+    # At beginning of move position system changes output file to /dev/null
+    # When the move is complete, the file is set a non-null value
+    try :
+        
+        if file_name == "/dev/null" :       
+            import json
+            with open(file_base_name + ".json") as json_file : metadata = json.load(json_file)
+            print("   metadata loaded")
+            tb.set_base_name("data")
+            metadata["run_time"] = time.time() - metadata["t_start"]
+            writeMetadata(metadata,file_base_name)
+            print("   base_name set to /dev/null")
+        else :
+            print("   Changing file_name to {0:s}".format(file_name))
+            metadata = buildMetadata('doppler','galactic_scan',tb)
+            metadata['t_start'] = time.time()    # this will not be as precise as time_catcher() but should be OK
+            file_base_name = args.dir + time.strftime("%Y-%m-%d-%H%M", time.gmtime())
+            writeMetadata(metadata,file_base_name)
+            tb.set_base_name(file_base_name)
+            print("   base_name set to {0:s}".format(file_base_name))
+        return True
+    except :
+        print("Exception in set_filename() ignored")
+        return False 
 
 def cur_sidereal(longitude):
     longstr = "%02d" % int(longitude)
@@ -142,6 +180,46 @@ def lmst_wait(target_lmst) :
             break
         time.sleep(20.0)
 
+def getData(file,fft_size) :
+    print("Reading from file: {0:s}".format(file))
+    vals = np.fromfile(file, dtype=np.float32)
+    cols = fft_size
+    rows = int(len(vals)/fft_size)
+    vals = np.reshape(vals, (rows,cols))   
+    return vals, rows, cols
+
+# convert .raw data format to .sum data format by summing over columns in the 
+# data array to make a time series of single values 
+def makeSumFile(base_name, metadata) :
+
+    with open(base_name + ".json") as json_file : metadata = json.load(json_file)
+
+    fft_size = metadata['fft_size']
+
+    for chan in [1,2] :
+        file = base_name + "_{0:d}.raw".format(chan)
+        data, nRows, nCols = getData(file,fft_size)
+        print("Read {0:d} {1:d}-channel spectra from {2:s}".format(nRows,nCols,file))
+        power = np.sum(data,1)
+        file_out = base_name + "_{0:d}.sum".format(chan)
+        with open(file_out,'w') as file : power.tofile(file)
+
+def makeAverageFile(base_name, metadata) :
+    fft_size = metadata['fft_size']
+
+    for chan in [1,2] :
+        data, rows, cols = getData(base_name + "_{0:d}.raw".format(chan),fft_size)
+        print("After getData Chan {0:d}: rows={1:d} cols={2:d}".format(chan,rows,cols))
+
+        # reshape array into a series of row 
+        data = np.reshape(data, (rows,cols))   
+        print("len(data[0])={0:d}".format(len(data[0])))
+
+        avg_data = np.mean(data,0)
+        print("len(avg_data)={0:d}".format(len(avg_data)))
+
+        avg_data.tofile(base_name + "_{0:d}.avg".format(chan))
+
 # begin execution
 
 args = getArgs() 
@@ -153,9 +231,13 @@ if run_mode == "pulsar" :
     f1, f2 = 1.4204e9, 1.4204e9
     fft_size = 32
     decimation_factor = 250
-    #samp_rate = 2.5e7
     samp_rate = f_clock/6.  
 elif run_mode == "doppler" :
+    f1, f2 = 1.4204e9, 1.4204e9
+    fft_size = 2048
+    decimation_factor = 10000
+    samp_rate = f_clock/30. 
+elif run_mode == "scan" :
     f1, f2 = 1.4185e9, 1.4185e9
     fft_size = 2048
     decimation_factor = 10000
@@ -164,7 +246,6 @@ elif run_mode == "generic" :
     f1, f2 = 1.4204e9, 1.4204e9
     fft_size = 2048
     decimation_factor = 10000
-    #samp_rate = 1.0e7
     samp_rate = f_clock/20. 
 else :
     print("**ERROR** invalid run_mode={0:s} ***Exiting***".format(run_mode))
@@ -176,44 +257,71 @@ if args.fft_size > 0 : fft_size = args.fft_size
 if args.decimation_factor > 0 : decimation_factor = args.decimation_factor
 if args.samp_rate > 0. : samp_rate = args.samp_rate  
 
-dir = args.dir 
-file_base_name = dir + time.strftime("%Y-%m-%d-%H%M", time.gmtime())
-
-print("Entering runDAQ: Run mode={0:s} file_base_name={1:s}".format(run_mode,file_base_name))
-
 if args.lmst != None : 
     print("Waiting for lmst={0:.2f}".format(args.lmst))
     lmst_wait(args.lmst)
     print("Starting")
 
-tb = DAQ(base_name=file_base_name, seconds=args.run_time, frequency=f1,  
-         fft_size=fft_size, decimation_factor=decimation_factor, samp_rate=samp_rate)
+# set up XMLRPC server so as to listen to control messages from 
+# the positioning system.   Create and run the XML server in a 
+# separate thread
 
-# this is not the right syntax 
-#desired_offset = 0.5*samp_rate + 1.0e5
-#tb.uhd.tune_request(f1,desired_offset)
+if args.XMLRPC :
+    from xmlrpc.server import SimpleXMLRPCServer
+    import threading
+    xmlserver = SimpleXMLRPCServer(('0.0.0.0', 14200), allow_none=True, logRequests=False)
+    xmlserver.register_function(set_filename)
+    server_thread = threading.Thread(target=xmlserver.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
 
-if printOn : print("Top block instantiated.")
+# allow for the possibility of a chain of jobs
+for i in range(args.n_jobs) :
 
-metadata = buildMetadata(args,tb)
+    file_base_name = args.dir + time.strftime("%Y-%m-%d-%H%M", time.gmtime())
 
-if printOn : print("Metadata built.  metadata={0:s}".format(str(metadata)))
+    print("In runDAQ: Run mode={0:s} file_base_name={1:s} {2:d} of {3:d}".format(
+    run_mode,file_base_name,i,args.n_jobs))
 
-print("Starting {0:s} flowgraph".format(run_mode))
-tb.start()
+    try :
+        tb = DAQ(base_name=file_base_name, seconds=args.run_time, frequency=f1,  
+            fft_size=fft_size, decimation_factor=decimation_factor, samp_rate=samp_rate)
+    except :
+        print("Error instantiating top_block.  Wait 10 seconds and try again.")
+        time.sleep(10.)
+        continue
 
-# let the flowgraph run a short time so allow the time stamp file to be written 
-time.sleep(2)
-metadata['t_start'] = float(open(file_base_name+"_ts.txt").readline()) 
-writeMetadata(metadata,file_base_name)
+    if printOn : print("Top block instantiated.")
 
-# this allows one to gracefully exit with Cntl+C 
-signal.signal(signal.SIGINT, signal_handler)
-print("\nRunning for {0:d} seconds.  Press Cntl+C to exit.".format(args.run_time)) 
+    metadata = buildMetadata(args.run_mode,args.target,tb)
 
-tb.wait()
-print("Flow stopped naturally after running for {0:d} seconds.".format(args.run_time))
-endRun(metadata,file_base_name)
+    if printOn : print("Metadata built.  metadata={0:s}".format(str(metadata)))
+
+    print("Starting {0:s} flowgraph".format(run_mode))
+    tb.start()
+
+    # let the flowgraph run a short time so allow the time stamp file to be written 
+    time.sleep(2)
+    metadata['t_start'] = float(open(file_base_name+"_ts.txt").readline()) 
+    writeMetadata(metadata,file_base_name)
+
+    # this allows one to gracefully exit with Cntl+C 
+    signal.signal(signal.SIGINT, signal_handler)
+    print("\nRunning for {0:d} seconds.  Press Cntl+C to exit.".format(args.run_time)) 
+
+    tb.wait()
+    print("Flow stopped naturally after running for {0:d} seconds.".format(args.run_time))
+    endRun(metadata,file_base_name)
+
+    if (run_mode == "pulsar" or run_mode == "scan") and not args.no_sum : makeSumFile(file_base_name, metadata)
+    if run_mode == "doppler" and not args.no_avg : 
+        makeAverageFile(file_base_name, metadata)
+        os.system("rm {0:s}_1.raw".format(file_base_name))
+        os.system("rm {0:s}_2.raw".format(file_base_name))
+
+    del tb
+    if args.n_jobs > 1 : time.sleep(10.)
+
 sys.exit(0)
 
 
